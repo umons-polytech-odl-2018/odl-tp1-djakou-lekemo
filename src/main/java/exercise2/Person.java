b69dc2438ab13950a71ac5c0c75643492e2af7e2
DJakou herve <172897@umons.ac.be>
2018-11-02 15:32:47 +0100

    exercice1 ok

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,22 @@
 package exercise2;
 
 class Person {
+	private static int populationSize = 0;
+	private static long totalAge = 0;
+	private int age;
+	public Person(int age){
+		this.age = age;
+		populationSize++;
+		totalAge += age;
+	}
+	public static int getPopulationSize(){
+		return populationSize;
+	}
+	public static float computePopulationAverage(){
+		return ((float)totalAge/populationSize);
+	}
+	public static void resetPopulation(){
+		populationSize = 0;
+		totalAge=0;
+	}
 }
